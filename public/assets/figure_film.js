474a86102691530176d8954bd773219c66ae49fa
Vincent Dumoulin <vi.dumoulin@gmail.com>
2017-11-13 12:26:53 -0500

    Add CNN subfigure

--- public/assets/figure_film.js
+++ public/assets/figure_film.js
@@ -13,6 +13,7 @@ function buildFigure () {
 
         // --- Style sub-figures ----------------------------------------------
         styleMLPFigure();
+        styleCNNFigure();
     }
 
     function styleMLPFigure () {
@@ -21,11 +22,11 @@ function buildFigure () {
 
         // --- Create data ----------------------------------------------------
         // Features
-        var features = [0.9, 0.0, -0.5, -0.8];
-        var gammas = [-1.6, -1.4, 0.8, 1.8];
-        var betas = [0.0, 1.3, 0.5, -0.5];
+        var features = [0.9, -0.5, -0.8];
+        var gammas = [-1.6, 0.8, 1.8];
+        var betas = [0.0, 0.5, -0.5];
         var data = [];
-        for (var i = 0; i < 4; i++) {
+        for (var i = 0; i < 3; i++) {
             data.push({
                 feature: features[i],
                 gamma: gammas[i],
@@ -46,9 +47,6 @@ function buildFigure () {
         // Scaling and shifting coefficients
 
         // --- Style figure elements ------------------------------------------
-        svg.select("g#mlp-figure text#gamma-label").style("font-size", 20);
-        svg.select("g#mlp-figure text#beta-label").style("font-size", 20);
-
         svg.select("g#mlp-figure g#input-layer").selectAll("g.feature").data(data);
         svg.select("g#mlp-figure g#gamma").selectAll("g.feature").data(data);
         svg.select("g#mlp-figure g#beta").selectAll("g.feature").data(data);
@@ -88,6 +86,98 @@ function buildFigure () {
             .on("mousemove", function (d, i) {
                 if (d3.event.buttons == 1) {
                     d.beta = mouseScale(d3.mouse(this)[1]);
+                    update();
+                }
+            });
+    }
+
+    function styleCNNFigure () {
+        // --- Retrieve svg element -------------------------------------------
+        var svg = d3.select("div.figure#film-diagram").select("svg");
+
+        // --- Create data ----------------------------------------------------
+        // Features
+        var features = [
+            [[-0.6, -0.6,  0.8], [ 0.6,  0.7,  0.4], [-0.8,  0.8,  0.9]],
+            [[ 0.9, -0.1,  0.5], [-0.8, -0.7,  0.5], [-0.3, -0.9, -0.2]],
+            [[-0.5, -0.9,  0.6], [-0.2, -0.4,  0.2], [-0.6,  0.1, -0.3]]
+        ];
+        var gammas = [-1.6, 0.8, 1.8];
+        var betas = [1.0, 0.5, -0.5];
+        var data = [];
+        var featureMapData = [[], [], []];
+        for (var k = 0; k < 3; k++) {
+            for (var i = 0; i < 3; i++) {
+                for (var j = 0; j < 3; j++) {
+                    var d = {
+                        feature: features[i][j][k],
+                        gamma: gammas[k],
+                        beta: betas[k],
+                    };
+                    data.push(d);
+                    featureMapData[2 - k].push(d);
+                }
+            }
+        }
+
+        var amplitudeScale = d3.scaleSqrt()
+            .domain([0.0, 2.0])
+            .range([0.0, 1.0])
+            .clamp(true);
+
+        var mouseScale = d3.scaleLinear()
+            .domain([0.0, 40.0])
+            .range([2.0, -2.0])
+            .clamp(true);
+
+        // Scaling and shifting coefficients
+
+        // --- Style figure elements ------------------------------------------
+        svg.select("g#cnn-figure g#input-layer").selectAll("g.feature").data(data);
+        svg.select("g#cnn-figure g#gamma").selectAll("g.feature").data(featureMapData);
+        svg.select("g#cnn-figure g#beta").selectAll("g.feature").data(featureMapData);
+        svg.select("g#cnn-figure g#scaled-layer").selectAll("g.feature").data(data);
+        svg.select("g#cnn-figure g#shifted-layer").selectAll("g.feature").data(data);
+
+        function updateSingle(selection, accessor, offset) {
+            selection.selectAll("g.feature").each(function (d, i) {
+                var r = accessor(d);
+                var s = Math.sign(r) * amplitudeScale(Math.abs(r));
+                d3.select(this)
+                  .select("g")
+                    .attr("transform", "matrix(" + [s, 0, 0, s, offset, offset] + ")");
+            });
+        }
+
+        function update() {
+            updateSingle(svg.select("g#cnn-figure g#input-layer"), function(d) { return d.feature; }, 15);
+            updateSingle(svg.select("g#cnn-figure g#gamma"), function(d) { return d[0].gamma; }, 20);
+            updateSingle(svg.select("g#cnn-figure g#beta"), function(d) { return d[0].beta; }, 20);
+            updateSingle(svg.select("g#cnn-figure g#scaled-layer"), function(d) { return d.gamma * d.feature; }, 15);
+            updateSingle(svg.select("g#cnn-figure g#shifted-layer"), function(d) { return d.gamma * d.feature + d.beta; }, 15);
+        }
+
+        update();
+
+        svg.select("g#cnn-figure g#gamma")
+          .selectAll("g.feature")
+            .on("mousemove", function (d) {
+                if (d3.event.buttons == 1) {
+                    var newValue = mouseScale(d3.mouse(this)[1]);
+                    for (var i = 0; i < 9; i++) {
+                        d[i].gamma = newValue;
+                    }
+                    update();
+                }
+            });
+        svg.select("g#cnn-figure g#beta")
+          .selectAll("g.feature")
+            .on("mousemove", function (d, i) {
+                if (d3.event.buttons == 1) {
+                    var newValue = mouseScale(d3.mouse(this)[1]);
+                    for (var i = 0; i < 9; i++) {
+                        d[i].beta = newValue;
+                    }
                     update();
                 }
             });
