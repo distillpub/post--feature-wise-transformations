3c060791f33d736b8ed2ec2c11ea170575bf59bb
Vincent Dumoulin <vi.dumoulin@gmail.com>
2017-11-13 16:45:17 -0500

    Fix t-SNE plot

--- public/assets/figure_clevr_tsne.js
+++ public/assets/figure_clevr_tsne.js
@@ -2,99 +2,96 @@ var colors = ["#8ed08b", "#afaed4", "#ecebf4", "#8d89c0", "#fdc692", "#fda057",
               "#89bedc", "#f67824", "#f44f39", "#fcaf93", "#fee3c8", "#d1d2e7",
               "#fc8161"];
 
-var buildFigure = function () {
-    // --- Create svg element -------------------------------------------------
-    var svg = d3.select("div.figure#clevr-tsne")
-      .append("svg")
-        // Width corresponds to the width of the body-outset class (744px)
-        .attr("viewBox", "0 0 744 500");
+function buildFigure () {
+    function styleFigure () {
+        // --- Retrieve svg element -------------------------------------------
+        var svg = d3.select("div.figure#clevr-tsne").select("svg");
 
-    var coordinates = function(d, i) {
-        switch (i) {
-            case 0: return d.layer_0;
-            case 1: return d.layer_1;
-            case 2: return d.layer_2;
-            case 3: return d.layer_3;
-            case 4: return d.layer_4;
-            case 5: return d.layer_5;
-        }
-    }
+        // --- Clear element-specific styling ---------------------------------
+        svg.selectAll(".figure-layer").style("fill", null);
+        svg.selectAll(".figure-network").style("fill", null);
+        svg.selectAll(".figure-text").style("font-size", null);
+        svg.selectAll(".figure-line").style("stroke", null);
+        svg.selectAll(".figure-path").style("fill", null);
+        svg.selectAll(".figure-operator").style("fill", null);
 
-    // --- Load data ----------------------------------------------------------
-    var dataset;
-    var xScales;
-    var yScales;
-    d3.json("assets/clevr_res_baseline6.json", function(data) {
-        dataset = data;
-        xScales = [];
-        yScales = [];
-        for (var i = 0; i < 6; i++) {
-            xScales.push(
-                d3.scaleLinear()
-                    .domain([d3.min(dataset, function (d) { return coordinates(d, i).x; }),
-                             d3.max(dataset, function (d) { return coordinates(d, i).x; })])
-                    .rangeRound([10, 490])
-            );
-            yScales.push(
-                d3.scaleLinear()
-                    .domain([d3.min(dataset, function (d) { return coordinates(d, i).y; }),
-                             d3.max(dataset, function (d) { return coordinates(d, i).y; })])
-                    .rangeRound([10, 490])
-            );
+        // --- Load data ------------------------------------------------------
+        var scatterPlot = svg.select("#scatter-plot");
+        var xMin = parseInt(scatterPlot.attr("x"));
+        var xMax = xMin + parseInt(scatterPlot.attr("width"));
+        var yMin = parseInt(scatterPlot.attr("y"));
+        var yMax = yMin + parseInt(scatterPlot.attr("height"));
+
+        var coordinates = function(d, i) {
+            switch (i) {
+                case 0: return d.layer_0;
+                case 1: return d.layer_1;
+                case 2: return d.layer_2;
+                case 3: return d.layer_3;
+                case 4: return d.layer_4;
+                case 5: return d.layer_5;
+            }
         }
 
-        svg.selectAll("circle")
-            .data(dataset)
-          .enter().append("circle")
-            .attrs({
-                "cx": function(d) { return xScales[0](d.layer_0.x); },
-                "cy": function(d) { return yScales[0](d.layer_0.y); },
-                "r": 4,
-            })
-            .style("fill", function(d) { return colors[d.question_type]; })
-            .style("opacity", 0.6)
-            .style("stroke", "#666")
-            .style("stroke-width", 1.5);
-    });
+        var dataset;
+        var xScales;
+        var yScales;
+        d3.json("assets/clevr_res_baseline6.json", function(data) {
+            dataset = data;
+            xScales = [];
+            yScales = [];
+            for (var i = 0; i < 6; i++) {
+                xScales.push(
+                    d3.scaleLinear()
+                        .domain([d3.min(dataset, function (d) { return coordinates(d, i).x; }),
+                                 d3.max(dataset, function (d) { return coordinates(d, i).x; })])
+                        .rangeRound([xMin, xMax])
+                );
+                yScales.push(
+                    d3.scaleLinear()
+                        .domain([d3.min(dataset, function (d) { return coordinates(d, i).y; }),
+                                 d3.max(dataset, function (d) { return coordinates(d, i).y; })])
+                        .rangeRound([yMin, yMax])
+                );
+            }
 
-    // --- Create UI elements -------------------------------------------------
-    var selectedButton = svg.append("rect")
-        .classed("figure-rect figure-rounded", true)
-        .attrs({
-            "x": 0, "y": 0, "width": 100, "height": 40,
-            "transform": "translate(520 260)",
+            svg.selectAll("circle")
+                .data(dataset)
+              .enter().append("circle")
+                .attrs({
+                    "cx": function(d) { return xScales[0](d.layer_0.x); },
+                    "cy": function(d) { return yScales[0](d.layer_0.y); },
+                    "r": 4,
+                })
+                .style("fill", function(d) { return colors[d.question_type]; })
+                .style("opacity", 0.6)
+                .style("stroke", "#666")
+                .style("stroke-width", 1.5);
         });
-    for (var k = 0; k < 6; k++) {
-        var button = svg.append("g")
-            .attrs({
-                "id": k,
-                "transform": "translate(520 " + (10 + (5 - k) * 50) + ")"
-            })
-            .on("click", function () {
-                var layer = parseInt(d3.select(this).attr("id"));
-                svg.selectAll("circle")
-                    .transition()
-                    .duration(500)
-                    .attrs({
-                        "cx": function(d) { return xScales[layer](coordinates(d, layer).x); },
-                        "cy": function(d) { return yScales[layer](coordinates(d, layer).y); }
-                    });
-                selectedButton
-                    .transition()
-                    .duration(500)
-                    .attrs({
-                        "transform": "translate(520 " + (10 + (5 - layer) * 50) + ")"
-                    });
-            });
-        button.append("rect")
-            .classed("figure-rect figure-rounded", true)
-            .attrs({"x": 0, "y": 0, "width": 100, "height": 40})
-            .style("stroke", "none");
-        button.append("text")
-            .classed("figure-text", true)
-            .attrs({"x": 50, "y": 20, "dy": "0.4em", "text-anchor": "middle"})
-            .text("Layer " + k);
+
+        // --- Program UI elements --------------------------------------------
+        for (var k = 0; k < 6; k++) {
+            svg.select("#film-layer-" + (k + 1))
+                .on("click", function () {
+                    var layer = parseInt(d3.select(this).attr("id").charAt(11)) - 1;
+                    svg.selectAll("circle")
+                        .transition()
+                        .duration(500)
+                        .attrs({
+                            "cx": function(d) { return xScales[layer](coordinates(d, layer).x); },
+                            "cy": function(d) { return yScales[layer](coordinates(d, layer).y); }
+                        });
+                });
+        }
     }
-};
+
+    d3.xml("assets/clevr_tsne.svg").mimeType("image/svg+xml").get(function(error, xml) {
+        if (error) throw error;
+        d3.select("div.figure#clevr-tsne").each(function () {
+            this.appendChild(xml.documentElement);
+        });
+        styleFigure();
+    });
+}
 
 buildFigure();
