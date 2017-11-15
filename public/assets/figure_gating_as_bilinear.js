d3bf9a94a8c4d1dce5fc41e2d18caa4ed2c015a5
Vincent Dumoulin <vi.dumoulin@gmail.com>
2017-11-15 16:49:58 -0500

    Commit WIP figures for gating and attention

---
+++ public/assets/figure_gating_as_bilinear.js
@@ -0,0 +1,22 @@
+function buildFigure () {
+    function styleFigure () {
+        // --- Retrieve svg element -------------------------------------------
+        var svg = d3.select("figure.figure#gating-as-bilinear-diagram").select("svg");
+
+        // --- Clear element-specific styling ---------------------------------
+        svg.selectAll(".figure-element, .figure-group, .figure-line, .figure-path")
+            .style("fill", null)
+            .style("stroke", null);
+        svg.selectAll(".figure-text").style("font-size", null);
+    }
+
+    d3.xml("assets/gating_as_bilinear.svg").mimeType("image/svg+xml").get(function(error, xml) {
+        if (error) throw error;
+        d3.select("figure.figure#gating-as-bilinear-diagram").each(function () {
+            this.insertBefore(xml.documentElement, this.firstChild);
+        });
+        styleFigure();
+    });
+}
+
+buildFigure();
