dd8366396b1b0bd915977c1294323c4fe0fe066d
Vincent Dumoulin <vi.dumoulin@gmail.com>
2017-11-18 10:05:54 -0500

    Experiment with article layout

--- public/assets/figure_gating_as_bilinear.js
+++ public/assets/figure_gating_as_bilinear.js
@@ -8,6 +8,52 @@ function buildFigure () {
             .style("fill", null)
             .style("stroke", null);
         svg.selectAll(".figure-text").style("font-size", null);
+
+        // --- Data -----------------------------------------------------------
+        var dataW1 = [];
+        var dataW2 = [];
+        var dataW3 = [];
+        for(var i = 0; i < 3; i++) {
+            dataW1.push(Math.random() * 360);
+            dataW2.push(Math.random() * 360);
+            dataW3.push(Math.random() * 360);
+        }
+
+        svg.select("#w-1")
+          .selectAll("path")
+            .data(dataW1)
+            .style("fill", function(d) { return "hsl(" + d + ",50%,50%)"; });
+        svg.select("#w-1-full")
+          .selectAll("path")
+            .filter(function() {
+               return !this.classList.contains('figure-faded')
+             })
+            .data(dataW1)
+            .style("fill", function(d) { return "hsl(" + d + ",50%,50%)"; });
+
+        svg.select("#w-2")
+          .selectAll("path")
+            .data(dataW2)
+            .style("fill", function(d) { return "hsl(" + d + ",50%,50%)"; });
+        svg.select("#w-2-full")
+          .selectAll("path")
+            .filter(function() {
+               return !this.classList.contains('figure-faded')
+             })
+            .data(dataW2)
+            .style("fill", function(d) { return "hsl(" + d + ",50%,50%)"; });
+
+        svg.select("#w-3")
+          .selectAll("path")
+            .data(dataW3)
+            .style("fill", function(d) { return "hsl(" + d + ",50%,50%)"; });
+        svg.select("#w-3-full")
+          .selectAll("path")
+            .filter(function() {
+               return !this.classList.contains('figure-faded')
+             })
+            .data(dataW3)
+            .style("fill", function(d) { return "hsl(" + d + ",50%,50%)"; });
     }
 
     d3.xml("assets/gating_as_bilinear.svg").mimeType("image/svg+xml").get(function(error, xml) {
