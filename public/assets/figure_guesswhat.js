d20385cec8a0d04e15988e00ac81679997ae5fe4
Vincent Dumoulin <vi.dumoulin@gmail.com>
2017-11-23 17:48:39 -0500

    Tweak GuessWhat?! diagram

--- public/assets/figure_guesswhat.js
+++ public/assets/figure_guesswhat.js
@@ -18,6 +18,49 @@ function buildFigure () {
                 "y": svg.select("#image-placeholder").attr("y"),
                 "href": "assets/guesswhat_input.jpg",
             });
+
+        // --- Hoverable content ----------------------------------------------
+        svg.select("g.film-generator")
+            .on("mouseenter", function () {
+                d3.select(this).select("g.film-generator > text")
+                    .text("FiLM generator");
+                d3.select(this).select("g.film-generator > rect")
+                    .style("fill", "#ef9a9a");
+            })
+            .on("mouseleave", function () {
+                d3.select(this).select("g.film-generator > text")
+                    .text("linguistic pipeline");
+                d3.select(this).select("g.film-generator > rect")
+                    .style("fill", null)
+            });
+
+        svg.selectAll("g.film-layer")
+            .on("mouseenter", function () {
+                d3.select(this).select("g.film-layer > text")
+                    .text("FiLM layer");
+                d3.select(this).select("g.film-layer > rect")
+                    .style("fill", "#ffcc80");
+            })
+            .on("mouseleave", function () {
+                d3.select(this).select("g.film-layer > text")
+                    .text("affine transformation");
+                d3.select(this).select("g.film-layer > rect")
+                    .style("fill", null)
+            });
+
+        svg.select("g.filmed-network")
+            .on("mouseenter", function () {
+                d3.select(this).select("g.filmed-network > text")
+                    .text("FiLM-ed network");
+                d3.select(this).select("g.filmed-network > rect")
+                    .style("fill", "#90caf9");
+            })
+            .on("mouseleave", function () {
+                d3.select(this).select("g.filmed-network > text")
+                    .text("visual pipeline");
+                d3.select(this).select("g.filmed-network > rect")
+                    .style("fill", null)
+            });
     }
 
     d3.xml("assets/guesswhat.svg").mimeType("image/svg+xml").get(function(error, xml) {
