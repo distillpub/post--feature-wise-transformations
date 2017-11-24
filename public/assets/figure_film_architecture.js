4acd059ec8e6386e0f04e2eda0b20ccaf742779d
Vincent Dumoulin <vi.dumoulin@gmail.com>
2017-11-24 14:08:21 -0500

    Make FiLM architecture figure hoverable

--- public/assets/figure_film_architecture.js
+++ public/assets/figure_film_architecture.js
@@ -8,6 +8,37 @@ function buildFigure () {
             .style("fill", null)
             .style("stroke", null);
         svg.selectAll(".figure-text").style("font-size", null);
+
+        // --- Hoverable content ----------------------------------------------
+        svg.selectAll("g.film-generator")
+            .on("mouseenter", function () {
+                d3.select(this).select("g.film-generator > rect")
+                    .style("fill", "#ef9a9a");
+            })
+            .on("mouseleave", function () {
+                d3.select(this).select("g.film-generator > rect")
+                    .style("fill", null)
+            });
+
+        svg.selectAll("g.film-layer")
+            .on("mouseenter", function () {
+                svg.selectAll("g.film-layer > rect")
+                    .style("fill", "#ffcc80");
+            })
+            .on("mouseleave", function () {
+                svg.selectAll("g.film-layer > rect")
+                    .style("fill", null)
+            });
+
+        svg.selectAll("g.filmed-network")
+            .on("mouseenter", function () {
+                d3.select(this).select("g.filmed-network > rect")
+                    .style("fill", "#90caf9");
+            })
+            .on("mouseleave", function () {
+                d3.select(this).select("g.filmed-network > rect")
+                    .style("fill", null)
+            });
     }
 
     d3.xml("assets/film_architecture.svg").mimeType("image/svg+xml").get(function(error, xml) {
