595c6f4f995cd68d35147e6626a4afd9a1078744
Vincent Dumoulin <vi.dumoulin@gmail.com>
2017-11-24 06:52:02 -0500

    Tweak several figures

--- public/assets/figure_gated_attention.js
+++ public/assets/figure_gated_attention.js
@@ -8,6 +8,52 @@ function buildFigure () {
             .style("fill", null)
             .style("stroke", null);
         svg.selectAll(".figure-text").style("font-size", null);
+
+        // --- Hoverable content ----------------------------------------------
+        d3.selectAll("path.figure-film-generator")
+            .style("fill", "#f44336")
+            .style("stroke", "#f44336");
+        svg.selectAll("g.film-generator")
+            .on("mouseenter", function () {
+                d3.select(this).select("g.film-generator > text")
+                    .text("FiLM generator");
+                d3.selectAll("g.film-generator > .figure-film-generator")
+                    .style("fill", "#ef9a9a");
+            })
+            .on("mouseleave", function () {
+                d3.select(this).select("g.film-generator > text")
+                    .text("");
+                d3.selectAll("g.film-generator > .figure-film-generator")
+                    .style("fill", null);
+            });
+
+        svg.select("g.film-layer")
+            .on("mouseenter", function () {
+                d3.select(this).select("g.film-layer > text")
+                    .text("FiLM layer");
+                d3.select(this).select("g.film-layer > .figure-film-layer")
+                    .style("fill", "#ffcc80");
+            })
+            .on("mouseleave", function () {
+                d3.select(this).select("g.film-layer > text")
+                    .text("");
+                d3.select(this).select("g.film-layer > .figure-film-layer")
+                    .style("fill", null);
+            });
+
+        svg.select("g.filmed-network")
+            .on("mouseenter", function () {
+                d3.select(this).select("g.filmed-network > text")
+                    .text("FiLM-ed layer");
+                d3.selectAll("g.filmed-network > .figure-filmed-network")
+                    .style("fill", "#90caf9");
+            })
+            .on("mouseleave", function () {
+                d3.select(this).select("g.filmed-network > text")
+                    .text("gated-attention multimodal fusion unit");
+                d3.selectAll("g.filmed-network > .figure-filmed-network")
+                    .style("fill", null);
+            });
     }
 
     d3.xml("assets/gated_attention.svg").mimeType("image/svg+xml").get(function(error, xml) {
