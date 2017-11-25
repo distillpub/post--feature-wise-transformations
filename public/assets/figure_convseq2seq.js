b16226047c17428e37aeabf01f3d11d03a9962c5
Vincent Dumoulin <vi.dumoulin@gmail.com>
2017-11-25 10:59:54 -0500

    Finish squeeze-and-excitation and GLU diagrams

--- public/assets/figure_convseq2seq.js
+++ public/assets/figure_convseq2seq.js
@@ -8,6 +8,49 @@ function buildFigure () {
             .style("fill", null)
             .style("stroke", null);
         svg.selectAll(".figure-text").style("font-size", null);
+
+        // --- Hoverable content ----------------------------------------------
+        svg.selectAll("g.film-generator")
+            .on("mouseenter", function () {
+                svg.select("text#film-generator-text")
+                    .text("FiLM generator");
+                svg.selectAll("g.film-generator > .figure-film-generator")
+                    .style("fill", "#ef9a9a");
+            })
+            .on("mouseleave", function () {
+                svg.select("text#film-generator-text")
+                    .text("");
+                svg.selectAll("g.film-generator > .figure-film-generator")
+                    .style("fill", null);
+            });
+
+        svg.select("g.film-layer")
+            .on("mouseenter", function () {
+                d3.select(this).select("g.film-layer > text")
+                    .text("FiLM layer");
+                svg.selectAll("g.film-layer > .figure-film-layer")
+                    .style("fill", "#ffcc80");
+            })
+            .on("mouseleave", function () {
+                d3.select(this).select("g.film-layer > text")
+                    .text("");
+                svg.selectAll("g.film-layer > .figure-film-layer")
+                    .style("fill", null);
+            });
+
+        svg.select("g.filmed-network")
+            .on("mouseenter", function () {
+                d3.select(this).select("g.filmed-network > text")
+                    .text("FiLM-ed layer");
+                svg.selectAll("g.filmed-network > .figure-filmed-network")
+                    .style("fill", "#90caf9");
+            })
+            .on("mouseleave", function () {
+                d3.select(this).select("g.filmed-network > text")
+                    .text("feature subset A");
+                svg.selectAll("g.filmed-network > .figure-filmed-network")
+                    .style("fill", null);
+            });
     }
 
     d3.xml("assets/convseq2seq.svg").mimeType("image/svg+xml").get(function(error, xml) {
