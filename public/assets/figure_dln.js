1c38d3d5385cb93a708aa926f9d19348a0a58c50
Vincent Dumoulin <vi.dumoulin@gmail.com>
2017-11-25 10:27:34 -0500

    Finish DLN figure

--- public/assets/figure_dln.js
+++ public/assets/figure_dln.js
@@ -8,6 +8,51 @@ function buildFigure () {
             .style("fill", null)
             .style("stroke", null);
         svg.selectAll(".figure-text").style("font-size", null);
+
+        // --- Hoverable content ----------------------------------------------
+        svg.selectAll("g.film-generator")
+            .on("mouseenter", function () {
+                svg.selectAll("g.film-generator > text")
+                    .text("FiLM gen.");
+                svg.selectAll("g.film-generator > .figure-film-generator")
+                    .style("fill", "#ef9a9a");
+            })
+            .on("mouseleave", function () {
+                svg.select("#film-gen-linear > text")
+                    .text("linear");
+                svg.select("#film-gen-utt-sum > text")
+                    .text("utt. sum.");
+                svg.selectAll("g.film-generator > .figure-film-generator")
+                    .style("fill", null);
+            });
+
+        svg.select("g.film-layer")
+            .on("mouseenter", function () {
+                svg.selectAll("g.film-layer > text")
+                    .text("FiLM");
+                svg.selectAll("g.film-layer > .figure-film-layer")
+                    .style("fill", "#ffcc80");
+            })
+            .on("mouseleave", function () {
+                svg.selectAll("g.film-layer > text")
+                    .text("affine");
+                svg.selectAll("g.film-layer > .figure-film-layer")
+                    .style("fill", null);
+            });
+
+        svg.select("g.filmed-network")
+            .on("mouseenter", function () {
+                d3.select(this).select("g.filmed-network > text")
+                    .text("FiLM-ed layer");
+                svg.selectAll("g.filmed-network > .figure-filmed-network")
+                    .style("fill", "#90caf9");
+            })
+            .on("mouseleave", function () {
+                d3.select(this).select("g.filmed-network > text")
+                    .text("LSTM layer");
+                svg.selectAll("g.filmed-network > .figure-filmed-network")
+                    .style("fill", null);
+            });
     }
 
     d3.xml("assets/dln.svg").mimeType("image/svg+xml").get(function(error, xml) {
